ca609f54ac079ad813679db16b5b160dd666b9e7
NotTolon <68058622+NotTolon@users.noreply.github.com>
2021-02-11 01:31:35 -0800

    mixins
    im tired gn

---
+++ src/main/java/org/faxhax/faxhax/mixin/FaxMixinLoader.java
@@ -0,0 +1,44 @@
+package org.faxhax.faxhax.mixin;
+
+import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
+import org.spongepowered.asm.launch.MixinBootstrap;
+import org.spongepowered.asm.mixin.MixinEnvironment;
+import org.spongepowered.asm.mixin.Mixins;
+
+import javax.annotation.Nullable;
+import java.util.Map;
+
+
+public class FaxMixinLoader implements IFMLLoadingPlugin {
+    public FaxMixinLoader() {
+        MixinBootstrap.init();
+
+        Mixins.addConfiguration("mixins.faxhax.json");
+
+        MixinEnvironment.getDefaultEnvironment().setObfuscationContext("searge");
+    }
+
+    @Override
+    public String[] getASMTransformerClass() {
+        return new String[0];
+    }
+
+    @Override
+    public String getModContainerClass() {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getSetupClass() {
+        return null;
+    }
+
+    @Override
+    public void injectData(Map<String, Object> data) {}
+
+    @Override
+    public String getAccessTransformerClass() {
+        return null;
+    }
+}
