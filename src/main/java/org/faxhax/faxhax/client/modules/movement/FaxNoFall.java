fe5e67715ca8d7765cd51da63d0ec22b61970fd4
RemainingToast <remainingtoast@gmail.com>
2021-02-15 04:27:18 +1000

    Added mode to NoFall

--- src/main/java/org/faxhax/faxhax/client/modules/movement/FaxNoFall.java
+++ src/main/java/org/faxhax/faxhax/client/modules/movement/FaxNoFall.java
@@ -2,18 +2,35 @@ package org.faxhax.faxhax.client.modules.movement;
 
 import net.minecraft.network.play.client.CPacketPlayer;
 import org.faxhax.faxhax.api.module.FaxModule;
+import org.faxhax.faxhax.api.setting.FaxSetting;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class FaxNoFall extends FaxModule {
-    
+
+    FaxSetting.Mode mode;
+
     public FaxNoFall() {
         super("No Fall", FaxCategory.Movement);
+        setHudInfo(mode.getValue());
+    }
+
+    @Override
+    public void setup() {
+        List<String> modes = new ArrayList<>();
+        modes.add("Packet");
+
+        mode = registerMode("Mode", modes, "Packet");
     }
 
     @Override
     public void onUpdate() {
         if (mc.player == null) return;
-        if (isFalling() && mc.player.fallDistance <= 3f) return;
-        mc.player.connection.sendPacket(new CPacketPlayer(true));
+        if(mode.getValue().equalsIgnoreCase("Packet")){
+            if (isFalling() && mc.player.fallDistance <= 3f) return;
+            mc.player.connection.sendPacket(new CPacketPlayer(true));
+        }
     }
 
     private boolean isFalling(){
